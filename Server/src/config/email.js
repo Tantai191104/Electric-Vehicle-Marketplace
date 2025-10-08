b2ee3c1580d42ac3c4c648f02fc4fcdff4c21917
taivippro123 <phanvothanhtai1007@gmail.com>
2025-10-08 19:45:17 +0700

    fix email

--- Server/src/config/email.js
+++ Server/src/config/email.js
@@ -9,6 +9,9 @@ export const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 // Verify connection configuration
